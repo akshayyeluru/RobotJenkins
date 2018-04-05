1afecd6f07478db60f7f155123ff57f0a37b446a
akshayyeluru <akshay.yeluru@gmail.com>
2018-04-05 17:12:43 +0530

    Updated CSVLibrary.py

--- CSVLibrary.py
+++ CSVLibrary.py
@@ -5,18 +5,18 @@ class CSVLibrary(object):
     def read_csv_file(self, path):
         book = xlrd.open_workbook(path)
         first_sheet = book.sheet_by_index(0)
-        numrows = first_sheet.nrows
-        numcols = first_sheet.ncols
+        numrowss = first_sheet.nrows
+        numcolss = first_sheet.ncols
         result_data = []
         headers = []
         # Forming headers
-        for j in range(0, numcols):
+        for j in range(0, numcolss):
             headers.append(first_sheet.cell_value(0, j))
 
-        for curr_row in range(1, numrows):
+        for curr_row in range(1, numrowss):
             row_data = []
             emp_data = {}
-            for curr_col in range(0, numcols):
+            for curr_col in range(0, numcolss):
                 data = first_sheet.cell_value(curr_row, curr_col)  # Read the data in the current cell
                 head = headers[curr_col]
                 emp_data[head.encode('ascii')] = str(data).encode('ascii')
